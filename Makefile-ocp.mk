22c4576097196497f2ca6ffcd3c9633055f08c19
Christoph Stäbler <cstabler@redhat.com>
2026-06-17 16:50:59 +0200

    UPSTREAM: <carry>: Konflux Tekton pipelines and Quay config

--- Makefile-ocp.mk
+++ Makefile-ocp.mk
@@ -1,3 +1,7 @@
+# Override the IMAGE_TAG_BASE from the upstream repo
+IMAGE_TAG_BASE ?= quay.io/opendatahub/odh-mcp-lifecycle-operator
+IMAGE_TAG ?= odh-stable
+
 include Makefile
 
 .PHONY: build-ocp
